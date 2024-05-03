dd4c8ed33b30eb208e70df6402c556a46a09d1ae
Lucas Oliveira Rodrigues <162646028+lucasolr8627@users.noreply.github.com>
2024-05-03 11:44:51 -0300

    Add files via upload
    Melhoria dos comentários no corpo do código

--- plotf.m
+++ plotf.m
@@ -1,4 +1,4 @@
-%% plotf - plot espectro de frequência
+%% plotf - plot espectro de amplitude
 
 % descrição dos parâmetros
 % X: vetor real
@@ -9,17 +9,30 @@
 function plotf(X, fa)
     % numero de amostras
     N = length(X);
-    % potencia de 2 mais proxima melhora a performance da fft
+    % usar N como potencia de 2 melhora a performance da fft
+    % calculamos a potência de 2 mais próxima, pouco alterando a faixa de
+    % frequência analisada
     N = pow2(nextpow2(N));
-    % calcula a fft do sinal
+    % calcula a fft do sinal passando N como segundo parâmetro
     X_fft = fft(X, N);
-    % normalização
+    % calcula o módulo da fft pois estamos interessados na correlação de
+    % senos (im) e cossenos (real) de cada frequência com os harmônicos do
+    % sinal
+    % dividimos por N para obter a amplitude
     A_X1 = abs(X_fft) / N;
-    % separa primeira metade e concentra o espectro
+    % separa a primeira metade (frequências positivas) pois o módulo da fft
+    % é espelhado e só nos interessa uma das metades
     A_X2 = A_X1(1:floor(N/2));
+    % concentra o espectro, multiplicando por 2 todas as amplitudes exceto
+    % 0Hz e a frequência de Nyquist (fa/2)
     A_X2(2:end-1) = 2*A_X2(2:end-1);
-    % faixa de frequência dos harmonicos
+    % faixa de frequência dos harmônicos vai de 0Hz até a frequência de
+    % Nyquist (length(A_X2) = floor(N/2), assim o último elemento de s é
+    % (N/2) * (fa/N) = fa/2)
     s = (0:length(A_X2) - 1) * (fa/N);
     % plot
-    plot(s, A_X2);
+    stem(s, A_X2, 'k');
+    xlabel('frequência (Hz)');
+    ylabel('Amplitude');
+    title('Espectro de Amplitude');
 end
